cb17f45e9130f6126f4bd1bbf5b03017d0e8066d
rocatoro18 <rocatoro18@outlook.com>
2021-11-28 02:23:51 -0700

    Modulo Comanda version 0.0.1 alpha Finalizado

--- src/controller/comandaController.js
+++ src/controller/comandaController.js
@@ -2,7 +2,7 @@ const express = require('express');
 const router = express.Router();
 const pool = require('../database');
 const {isLoggedIn} = require('../lib/auth');
-const producto = require('../model/comanda');
+const comanda = require('../model/comanda');
 
 
 module.exports = {
@@ -10,15 +10,22 @@ module.exports = {
         /*producto.obtener(pool,function (err,datos) {
             res.render('productos/list',{productos:datos});
         });*/
+        comanda.obtener(pool,function (err,datos){
+            res.render('comandas/list',{comanda:datos});
+        });
     },
     addComanda:function (req,res) {
-        //res.render('productos/add');
+        res.render('comandas/add');
     },
     editComanda:function (req,res) {
         /*const {id} = req.params;
         producto.editar(pool,id,function (err,datos) {
             res.render('productos/edit',{producto:datos[0]});
         });*/
+        const {id} = req.params;
+        comanda.editar(pool,id,function (err,datos){
+            res.render('comandas/edit',{comanda:datos[0]});
+        });
     },
     deleteComanda:function (req,res) {
         /*const {id} = req.params;
@@ -26,6 +33,11 @@ module.exports = {
             req.flash('success','Eliminado con éxito');
             res.redirect('/productos');
         });*/
+        const {id} = req.params;
+        comanda.eliminar(pool,id, function (err,datos){
+            req.flash('success','Eliminado con éxito');
+            res.redirect('/comandas');
+        });
     },
     insertarComanda:function (req,res) {
        /* const {nombre_producto, cantidad_producto, precio_compra, precio_venta} = req.body;
@@ -40,6 +52,19 @@ module.exports = {
             req.flash('success', 'Guardado con éxito');
             res.redirect('/productos');
         });*/
+        const {id} = req.params;
+        const {numero_mesa,numero_personas,anotacion_comanda,cantidad_comanda,importe_comanda} = req.body;
+        const newComanda = {
+            numero_mesa,
+            numero_personas,
+            anotacion_comanda,
+            cantidad_comanda,
+            importe_comanda
+        };
+        comanda.insertar(pool,newComanda,function (err){
+            req.flash('success','Guardado con éxito');
+            res.redirect('/comandas');
+        });
     },
     editarComanda:function (req,res){
        /* const {id} = req.params;
@@ -54,5 +79,18 @@ module.exports = {
             req.flash('success','Actualizado con éxito');
             res.redirect('/productos');
         });*/
+        const {id} = req.params;
+        const {numero_mesa,numero_personas,anotacion_comanda,cantidad_comanda,importe_comanda} = req.body;
+        const newComanda = {
+            numero_mesa,
+            numero_personas,
+            anotacion_comanda,
+            cantidad_comanda,
+            importe_comanda
+        };
+        comanda.editarComanda(pool,id,newComanda,function (err){
+            req.flash('success','Actualizado con éxito');
+            res.redirect('/comandas');
+        });
     }
 }
--- src/model/comanda.js
+++ src/model/comanda.js
@@ -1,18 +1,24 @@
 module.exports = {
     obtener:function (conexion,funcion) {
     //    conexion.query('SELECT * FROM PRODUCTOS'/*WHERE ID_USUARIO = ?',[req.user.ID_USUARIO]*/,funcion);
+          conexion.query('SELECT * FROM COMANDAS',funcion);
     },
     editar:function (conexion,reqID,funcion) {
         //const {id} = req.params;
     //    conexion.query('SELECT * FROM PRODUCTOS WHERE ID_PRODUCTO = ?',[reqID],funcion);
+          //const {id} = req.params;
+          conexion.query('SELECT * FROM COMANDAS WHERE ID_COMANDA = ?',[reqID],funcion);
     },
     eliminar:function (conexion,reqID,funcion) {
     //    conexion.query('DELETE FROM PRODUCTOS WHERE ID_PRODUCTO = ?',[reqID],funcion);
+          conexion.query('DELETE FROM COMANDAS WHERE ID_COMANDA = ?',[reqID],funcion);
     },
-    insertar:function (conexion,newProducto,funcion) {
+    insertar:function (conexion,newComanda,funcion) {
     //    conexion.query('INSERT INTO PRODUCTOS SET ?',[newProducto],funcion);
+          conexion.query('INSERT INTO COMANDAS SET ?',[newComanda],funcion);
     },
-    editarProducto:function (conexion,id,newProducto,funcion){
+    editarComanda:function (conexion,id,newComanda,funcion){
     //    conexion.query('UPDATE PRODUCTOS SET ? WHERE ID_PRODUCTO = ?',[newProducto,id],funcion);
+          conexion.query('UPDATE COMANDAS SET ? WHERE ID_COMANDA = ?',[newComanda,id],funcion);
     }
 }
